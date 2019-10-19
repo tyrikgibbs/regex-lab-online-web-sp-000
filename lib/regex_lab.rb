69514ec26f8acfa0918e1a31d2c7c64e4a8d9cab
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-19 20:24:12 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+    return true
+  else
+    return false
+  end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
+def words_starting_with_un_and_ending_with_ing(words_string)
+  words_string.scan(/\bun\w+ing\b/)
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(words_string)
+  words_string.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\.$/) == nil
+   return false
+ else
+   return true
+ end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+  else
+    return true
+  end
 end
