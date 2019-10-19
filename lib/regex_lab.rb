60dacdcdc84e24f765b12e80d9d16cd87572755c
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-19 20:01:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+    return true
+  else
+    return false
+  end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
+def words_starting_with_un_and_ending_with_ing(words_string)
+  words_string.scan(/\bun\w+ing\b/)
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(words_string)
+  words_string.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
 end
 
-def valid_phone_number?(phone)
-
+def valid_phone_number?(number)
+  if number.scan(/\d+\d+\d/)
+    return true
+  else
+    return false
+  end
 end
