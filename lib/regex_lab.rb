1ac2b2a22330cf96b9f2af050c690aab1819754e
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-19 20:22:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,9 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(number)
-  if number.scan(/\d+\d+\d/)
-    return true
-  else
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
     return false
+  else
+    return true
   end
 end
